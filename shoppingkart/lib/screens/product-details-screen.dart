0cf94de2df4191e1a2305c85275b936ee8dc7e96
Shouvik Bhuiyan <shouvikbhuiyan@Ambalikas-MacBook-Pro.local>
2020-06-27 09:13:29 +0530

    shhopping ekart application

--- shoppingkart/lib/screens/product-details-screen.dart
+++ shoppingkart/lib/screens/product-details-screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/productDetails.dart';
+import '../provider/products.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static String routeName = '/product-details';
+  
 
   @override
   Widget build(BuildContext context) {
+  final args =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+  final ProductItem providerPItem = Provider.of<ProductDetails>(context).getProductById(args['id']);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product Details'),
+        title: Text(providerPItem.title),
       ),
     );
   }
--- shoppingkart/lib/widgets/product-grid-item.dart
+++ shoppingkart/lib/widgets/product-grid-item.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../screens/product-details-screen.dart';
 import '../provider/products.dart';
 
 class ProductGridItem extends StatelessWidget {
+  void _navigateToDetails(ctx, id) {
+    Navigator.pushNamed(
+      ctx,
+      ProductDetailsScreen.routeName,
+      arguments: <String, String>{'id': id},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var providerData = Provider.of<ProductItem>(context);
 
     return GestureDetector(
-      onTap: () {},
+      onTap: () => _navigateToDetails(context, providerData.id),
       child: GridTile(
         child: Image.network(
           providerData.imageUrl,
@@ -17,8 +27,10 @@ class ProductGridItem extends StatelessWidget {
         footer: GridTileBar(
           backgroundColor: Colors.black54,
           leading: IconButton(
-            icon: Icon(Icons.favorite_border),
-            onPressed: () {},
+            icon: Icon(providerData.isFavourite
+                ? Icons.favorite
+                : Icons.favorite_border),
+            onPressed: providerData.togglePreference,
           ),
           title: Text(providerData.title),
           trailing: IconButton(
