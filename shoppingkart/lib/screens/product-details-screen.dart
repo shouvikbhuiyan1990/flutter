7c05a4461af8de81698a4fe3119429d99c35edba
Shouvik Bhuiyan <shouvikbhuiyan@Ambalikas-MacBook-Pro.local>
2020-06-28 11:35:17 +0530

    shhopping ekart application

--- shoppingkart/lib/screens/product-details-screen.dart
+++ shoppingkart/lib/screens/product-details-screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:badges/badges.dart';
 
+import '../provider/cart.dart';
 import '../provider/productDetails.dart';
 import '../provider/products.dart';
+import '../screens/cart-details-screen.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static String routeName = '/product-details';
@@ -15,9 +18,41 @@ class ProductDetailsScreen extends StatelessWidget {
         Provider.of<ProductDetails>(context, listen: false)
             .getProductById(args['id']);
 
+    final cartProvider = Provider.of<Cart>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(providerPItem.title),
+        actions: <Widget>[
+          Consumer<Cart>(
+            builder: (_, cart, child) {
+              return Padding(
+                padding: EdgeInsets.only(right: 40),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pushNamed(
+                      CartDetails.routeName,
+                    );
+                  },
+                  child: Badge(
+                    borderRadius: 10,
+                    position: BadgePosition.topLeft(
+                      top: 4,
+                      left: 12,
+                    ),
+                    badgeContent: Text(
+                      cart.getTotalCartLengthQuantity.toString(),
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: Icon(Icons.shopping_cart),
+                  ),
+                ),
+              );
+            },
+          )],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -31,8 +66,10 @@ class ProductDetailsScreen extends StatelessWidget {
               ),
             ),
             Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              width: double.infinity,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
                     providerPItem.title,
@@ -51,10 +88,23 @@ class ProductDetailsScreen extends StatelessWidget {
                   SizedBox(
                     height: 20,
                   ),
-                  RaisedButton(
-                    child: Text('Add to Cart'),
-                    onPressed: () {},
-                  )
+                  Container(
+                    width: double.infinity,
+                    child: Column(
+                      children: <Widget>[
+                        RaisedButton(
+                          child: Text('Add to Cart'),
+                          onPressed: () {
+                            cartProvider.addNewCartItem(
+                              providerPItem.id,
+                              providerPItem.title,
+                              providerPItem.price,
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             )
